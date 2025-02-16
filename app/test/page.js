fbcfe188944dc507325097bfe21bcfe14776b8c0
battlesofarmy <battlesofarmy@gmail.com>
2025-02-17 00:00:21 +0600

    banner-5 added

--- app/test/page.js
+++ app/test/page.js
@@ -1,29 +1,18 @@
-import Image from 'next/image'
-
-export default function Banner_4() {
+export default function Banner_5() {
   return (
-    <section className="p-6 dark:bg-gray-100 dark:text-gray-800">
-  <div className="container grid gap-6 mx-auto text-center lg:grid-cols-2 xl:grid-cols-5">
-    <div className="w-full px-6 py-16 rounded-md sm:px-12 md:px-16 xl:col-span-2 dark:bg-gray-50 bg-gray-200">
-      <span className="block mb-2 dark:text-default-600">Mamba design system</span>
-      <h1 className="text-5xl font-extrabold dark:text-gray-900">Build it with Mamba</h1>
-      <p className="my-8">
-        <span className="font-medium dark:text-gray-900">Modular and versatile.</span>Fugit vero facilis dolor sit neque cupiditate minus esse accusamus cumque at.
-      </p>
-      <form noValidate="" action="" className="self-stretch space-y-3">
-        <div>
-          <label htmlFor="name" className="text-sm sr-only">Your name</label>
-          <input id="name" type="text" placeholder="Your name" className="w-full rounded-md focus:ring focus:dark:ring-default-600 dark:border-gray-300" />
-        </div>
-        <div>
-          <label htmlFor="lastname" className="text-sm sr-only">Email address</label>
-          <input id="lastname" type="text" placeholder="Email address" className="w-full rounded-md focus:ring focus:dark:ring-default-600 dark:border-gray-300" />
-        </div>
-        <button type="button" className="w-full py-2 font-semibold rounded dark:bg-default-600 dark:text-gray-50">Join the waitlist</button>
-      </form>
-    </div>
-    <Image width={600} height={500} src="https://mambaui.com/assets/svg/Business_SVG.svg" alt="" className="object-cover w-full rounded-md xl:col-span-3 dark:bg-gray-500" />
-  </div>
-</section>
+	<>
+		<section style={{background: `url('https://security-seven-ruddy.vercel.app/_next/static/media/banner.c5c152c2.jpeg')no-repeat center center`}}>
+			<div className="bg-black/60 lg:py-40 md:py-28 py-20 text-white text-center">
+				<div className="container">
+					<h2 className="text-4xl">Muntasir Ahmed Fahim</h2>
+					<p className="mt-4 w-2/3 mx-auto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione incidunt distinctio earum placeat officiis non quisquam molestiae amet consectetur similique.</p>
+          <div className="flex gap-10 mt-8 justify-center">
+            <button className="text py-2 px-5 rounded bg-secondary">Learn More</button>
+            <button className="text py-2 px-5 rounded bg-secondary">Learn More</button>
+          </div>
+				</div>
+			</div>
+		</section>
+	</>
   )
 }

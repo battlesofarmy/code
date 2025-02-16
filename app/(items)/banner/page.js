1e85a82a120ec768474de81f0acb34fe84d01562
battlesofarmy <battlesofarmy@gmail.com>
2025-02-16 23:22:50 +0600

    another banner added

--- app/(items)/banner/page.js
+++ app/(items)/banner/page.js
@@ -1,9 +1,15 @@
 import Display from "@/components/displayFormet/Display";
 import Banner_1 from "./(elements)/Banner_1";
+import Banner_2 from "./(elements)/Banner_2";
+import Banner_3 from "./(elements)/Banner_3";
+import Banner_4 from "./(elements)/Banner_4";
 
 export default async function Card() {
 const components = {
 	0 : ()=> <Banner_1/>,
+	1 : ()=> <Banner_2/>,
+	2 : ()=> <Banner_3/>,
+	3 : ()=> <Banner_4/>,
 }
 
   return (

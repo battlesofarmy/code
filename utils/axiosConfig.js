2eaa330f8b61d8299032cebfaeb6fa6ecc9c5b55
battlesofarmy <battlesofarmy@gmail.com>
2025-02-17 17:24:13 +0600

    backend url changed

--- utils/axiosConfig.js
+++ utils/axiosConfig.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000", // Replace with your API base URL
-  // baseURL: "https://code-back-battlesofarmys-projects.vercel.app", // Replace with your API base URL
+  // baseURL: "http://localhost:5000", // Replace with your API base URL
+  baseURL: "https://code-back.vercel.app", // Replace with your API base URL
   timeout: 10000, // Optional: Set timeout (in ms)
   headers: { "Content-Type": "application/json" }, // Optional: Default headers
 });
